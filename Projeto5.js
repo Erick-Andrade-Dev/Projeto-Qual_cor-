4217860e34e48b9be190bb252977a2b9bbe0d76c
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-07-16 19:57:34 -0300

    adicionando resultado caso a pessoa acertar ou errar

--- Projeto5.js
+++ Projeto5.js
@@ -1,7 +1,9 @@
+let colorRbg = 6
+
 
 window.onload = function(){
     
- function acerteCor(){
+ function randomRgb(){
 
     let r = Math.floor(Math.random()* 255) 
     let g = Math.floor(Math.random()* 255);
@@ -9,35 +11,70 @@ window.onload = function(){
     let rgb = `rgb(${r}, ${g}, ${b})`
     
     return rgb
-   }          
-
-
-function backgroundColor(){
-
-    let listaRgb = [];
-
-   for (let i = 0; i < 6; i += 1)  {
+   }    
+   
+ let listaRgb = []; 
+ function backgroundColor(){
+    for (let i = 0; i < colorRbg; i += 1)  {
        let color = document.createElement('span')
         color.className = 'color'
-        color.style.backgroundColor = acerteCor();
+        color.style.backgroundColor = randomRgb();
+        listaRgb.push(color.style.backgroundColor)         
+        document.querySelector('.containerColor').appendChild(color)         
+    }             
 
-        listaRgb.push(color.style.backgroundColor)       
-        
-        document.querySelector('.containerColor').appendChild(color) 
-
-        const bntAlterar = document.querySelector('#btnReset')
-
-        bntAlterar.addEventListener('click', function(){           
-            color.style.backgroundColor = acerteCor();
-        })        
-    }
     let rgbAleatorio = Math.floor(Math.random() * 6)
     let colorSelect = listaRgb[rgbAleatorio]
     colorSelect = colorSelect.replace(/rgb/gi, '');
-    document.querySelector('#corRgb').innerHTML = colorSelect
+    document.querySelector('#corRgb').innerHTML = colorSelect    
+    console.log('listaRgb', listaRgb)
 } 
+
 backgroundColor();
 
+function btnReset(){
 
+const bntAlterar = document.querySelector('#btnReset')
+bntAlterar.addEventListener('click', function(){           
+        let corBtnReset = [];
+        let container = document.querySelectorAll('.color')         
+        
+        for(i = 0; i < container.length; i += 1){
+            let test = container[i].style.backgroundColor = randomRgb();  
+            test = test.replace(/rgb/gi, '');   
+            corBtnReset.push(test)
+        }
+        let rgbAleatorio = Math.floor(Math.random() * 6)
+        let corRbg = document.querySelector("#corRgb")
+        corRbg.innerText = corBtnReset[rgbAleatorio]   
+        document.querySelector('#resultado').innerText = '' 
+        resultColor()    
+    })
+}
+btnReset();
+
+
+function resultColor(){
+    let colorOne = document.querySelector('#corRgb').innerText   
+
+    let divColors = document.querySelector(".containerColor")    
+    divColors.addEventListener('click', function(event){
+        let boxColor = event.target.style.backgroundColor
+        boxColor = boxColor.replace(/rgb/gi, '');
+
+        verifColor(colorOne, boxColor);
+    })    
+}
+resultColor();
+
+function verifColor(boxColor, colorOne){
+    console.log(boxColor, colorOne)
+    if(boxColor == colorOne) {
+        document.querySelector('#resultado').innerText = 'Acertouuuuu!'
+        //document.querySelector('#result').innerText = +10
+    } else {
+        document.querySelector('#resultado').innerText = 'Game Over!'
+    }
 }
 
+}
